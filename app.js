e5281e0e9ab9cecc0eda89002ae040893d4337a9
mattdono-CU <matthew.m.donoghue@gmail.com>
2019-02-03 12:15:17 -0500

    Project setup files

---
+++ app.js
@@ -0,0 +1,25 @@
+require('aframe');
+require('aframe-event-set-component');
+
+//app setup
+const express   = require('express');
+const app       = express();
+const http      = require('http');
+const server    = http.createServer(app);
+
+//const vars
+const LISTEN_PORT = 8080;
+
+//middleware
+app.use(express.static(__dirname + '/public'));
+
+//set routes
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+//!!TRY: create another route to point to another web page
+
+//start server
+server.listen(LISTEN_PORT);
+console.log('Listening on port: ' + LISTEN_PORT );
